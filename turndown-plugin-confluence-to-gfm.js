a8c3b5e4b2a335e661b75d914b9fa6123d552779
guyplusplus <>
2021-04-15 15:50:07 +0700

    First check-in

---
+++ turndown-plugin-confluence-to-gfm.js
@@ -0,0 +1,73 @@
+var turndownPluginConfluence = (function (exports) {
+'use strict';
+
+var highlightRegExp = /brush: ([a-z0-9]+);/;
+
+function confluenceHighlightedCodeBlock (turndownService) {
+  turndownService.addRule('highlightedConfluenceCodeBlock', {
+    filter: function (node) {
+      var firstChild = node.firstChild;
+      return (
+        node.nodeName === 'DIV' &&
+        firstChild &&
+        firstChild.nodeName === 'PRE' &&
+        firstChild.className === 'syntaxhighlighter-pre'
+      )
+    },
+    replacement: function (content, node, options) {
+      var dataSyntaxhighlighterParams = node.firstChild.getAttribute('data-syntaxhighlighter-params') || '';
+      var language = (dataSyntaxhighlighterParams.match(highlightRegExp) || [null, ''])[1];
+
+      //@TODO: implement fenceInCodeRegex
+      return (
+        '\n\n' + options.fence + language + '\n' +
+        node.firstChild.textContent +
+        '\n' + options.fence + '\n\n'
+      )
+    }
+  });
+}
+
+function confluenceTaskListItems (turndownService) {
+  turndownService.addRule('confluenceTaskListItems', {
+    filter: function (node) {
+      return (
+        node.nodeName === 'LI' &&
+        node.parentNode.nodeName === 'UL' &&
+        node.parentNode.className === 'inline-task-list'
+      )
+    },
+    replacement: function (content, node) {
+      return (node.className === 'checked' ? '- [x]' : '- [ ]') + ' ' + content + '\n\n'
+    }
+  });
+}
+
+function confluenceSmiley (turndownService) {
+  turndownService.addRule('confluenceSmiley', {
+    filter: function (node) {
+      var className = node.className || '';
+      return className.includes('emoticon') && node.nodeName === 'IMG'
+    },
+    replacement: function (content, node) {
+      return (node.getAttribute('data-emoji-shortname') || '')
+    }
+  });
+}
+
+function confluenceGfm (turndownService) {
+  turndownService.use([
+    confluenceHighlightedCodeBlock,
+    confluenceTaskListItems,
+    confluenceSmiley
+  ]);
+}
+
+exports.confluenceGfm = confluenceGfm;
+exports.confluenceHighlightedCodeBlock = confluenceHighlightedCodeBlock;
+exports.confluenceTaskListItems = confluenceTaskListItems;
+exports.confluenceSmiley = confluenceSmiley;
+
+return exports;
+
+}({}));
---
+++ turndown-plugin-gfm-tmp.js
@@ -0,0 +1,164 @@
+var turndownPluginGfm = (function (exports) {
+'use strict';
+
+var highlightRegExp = /highlight-(?:text|source)-([a-z0-9]+)/;
+
+function highlightedCodeBlock (turndownService) {
+  turndownService.addRule('highlightedCodeBlock', {
+    filter: function (node) {
+      var firstChild = node.firstChild;
+      return (
+        node.nodeName === 'DIV' &&
+        highlightRegExp.test(node.className) &&
+        firstChild &&
+        firstChild.nodeName === 'PRE'
+      )
+    },
+    replacement: function (content, node, options) {
+      var className = node.className || '';
+      var language = (className.match(highlightRegExp) || [null, ''])[1];
+
+      return (
+        '\n\n' + options.fence + language + '\n' +
+        node.firstChild.textContent +
+        '\n' + options.fence + '\n\n'
+      )
+    }
+  });
+}
+
+function strikethrough (turndownService) {
+  turndownService.addRule('strikethrough', {
+    filter: ['del', 's', 'strike'],
+    replacement: function (content) {
+      return '~' + content + '~'
+    }
+  });
+}
+
+var indexOf = Array.prototype.indexOf;
+var every = Array.prototype.every;
+var rules = {};
+
+rules.tableCell = {
+  filter: ['th', 'td'],
+  replacement: function (content, node) {
+    return cell(content, node)
+  }
+};
+
+rules.tableRow = {
+  filter: 'tr',
+  replacement: function (content, node) {
+    var borderCells = '';
+    var alignMap = { left: ':--', right: '--:', center: ':-:' };
+
+    if (isHeadingRow(node)) {
+      for (var i = 0; i < node.childNodes.length; i++) {
+        var border = '---';
+        var align = (
+          node.childNodes[i].getAttribute('align') || ''
+        ).toLowerCase();
+
+        if (align) border = alignMap[align] || border;
+
+        borderCells += cell(border, node.childNodes[i]);
+      }
+    }
+    return '\n' + content + (borderCells ? '\n' + borderCells : '')
+  }
+};
+
+rules.table = {
+  // Only convert tables with a heading row.
+  // Tables with no heading row are kept using `keep` (see below).
+  filter: function (node) {
+    var b = node.nodeName === 'TABLE' && isHeadingRow(node.rows[0])
+    return b
+  },
+
+  replacement: function (content) {
+    // Ensure there are no blank lines
+    content = content.replace('\n\n', '\n');
+    return '\n\n' + content + '\n\n'
+  }
+};
+
+rules.tableSection = {
+  filter: ['thead', 'tbody', 'tfoot'],
+  replacement: function (content) {
+    return content
+  }
+};
+
+// A tr is a heading row if:
+// - the parent is a THEAD, assumed syntax with TH is correct
+// - or if its the first child of the TABLE or the first TBODY
+//   and every cell is a TH
+function isHeadingRow (tr) {
+  var parentNode = tr.parentNode;
+  if(parentNode.nodeName === 'THEAD')
+    return true
+  if(parentNode.nodeName === 'TFOOT')
+    return false
+  if(parentNode.nodeName === 'TBODY') {
+    var parentParentNode = parentNode.parentNode;
+    if(parentParentNode.nodeName !== 'TABLE')
+      return false;
+    if(parentParentNode.rows[0] !== tr)
+      return false;
+    return every.call(tr.childNodes, function (n) { return n.nodeName === 'TH' })
+  }
+  //no THEAD, TBODY nor TFOOT
+  if(parentNode.nodeName !== 'TABLE')
+    return false;
+  if(parentNode.rows[0] !== tr)
+    return false;
+  return every.call(tr.childNodes, function (n) { return n.nodeName === 'TH' })
+}
+
+function cell (content, node) {
+  var index = indexOf.call(node.parentNode.childNodes, node);
+  var prefix = ' ';
+  if (index === 0) prefix = '| ';
+  // Ensure single line per cell
+  content = content.replace(/\n/g, ' ');
+  return prefix + content + ' |'
+}
+
+function tables (turndownService) {
+  turndownService.keep(function (node) {
+    return node.nodeName === 'TABLE' && !isHeadingRow(node.rows[0])
+  });
+  for (var key in rules) turndownService.addRule(key, rules[key]);
+}
+
+function taskListItems (turndownService) {
+  turndownService.addRule('taskListItems', {
+    filter: function (node) {
+      return node.type === 'checkbox' && node.parentNode.nodeName === 'LI'
+    },
+    replacement: function (content, node) {
+      return (node.checked ? '[x]' : '[ ]') + ' '
+    }
+  });
+}
+
+function gfm (turndownService) {
+  turndownService.use([
+    highlightedCodeBlock,
+    strikethrough,
+    tables,
+    taskListItems
+  ]);
+}
+
+exports.gfm = gfm;
+exports.highlightedCodeBlock = highlightedCodeBlock;
+exports.strikethrough = strikethrough;
+exports.tables = tables;
+exports.taskListItems = taskListItems;
+
+return exports;
+
+}({}));
