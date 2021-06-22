b71a1ba1c4d07bf50fe62f488184f986cdf88cb4
guyplusplus <18182742+guyplusplus@users.noreply.github.com>
2021-06-22 23:08:05 +0700

    improved class search

--- src/confluence-smiley.js
+++ src/confluence-smiley.js
@@ -1,8 +1,8 @@
 export default function confluenceSmiley (turndownService) {
   turndownService.addRule('confluenceSmiley', {
     filter: function (node) {
-      var className = node.className || ''
-      return className.includes('emoticon') && node.nodeName === 'IMG'
+      var classes = (node.className || '').split(' ')
+      return classes.includes('emoticon') && node.nodeName === 'IMG'
     },
     replacement: function (content, node) {
       return (node.getAttribute('data-emoji-shortname') || '')
